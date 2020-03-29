6676a72e1cb4d62320a7dc31091ec4bcc0dbe942
Julia.Zorina <julia.zorinaa@gmail.com>
2020-03-29 16:45:44 +0300

    added changeColor

--- mag.js
+++ mag.js
@@ -1,6 +1,5 @@
 
 let signBtn = document.getElementById('signbtn');
-/* let bgBox = document.getElementsByClassName('bgbox'); */
 let hide = document.getElementById('movebox');
 let show = document.getElementById('showbox');
 let blockNoAccount = document.getElementsByClassName('blockone')[0];
@@ -9,39 +8,50 @@ signBtn.addEventListener('click', moveLeft);
 signBtn.addEventListener('click', changeColor);
 
 function changeColor(){  
-    let pos = 0;
-/*     let positionright = 0;  */
+   /*  let pos = 0;
     let id = setInterval(frame, 5);    
     function frame(){
     let point = document.getElementById('throw');
-    if(pos ==  200){ 
-/*   clearInterval(id);
-} else { */
+    if(pos == 200){ 
+ clearInterval(id);
+} else { 
    pos++;
     point.style.right = pos + 'px';
-    /* point.style.top = pos + 'px'; */
+    point.style.top = pos + 'px'; 
     signBtn.classList.add('color_selected');
-    }   
+    }    */
+    let point = document.getElementById('throw');
+    var pos = 0;
+    var id = setInterval(frame,5);
+    function frame(){ 
+    if(pos == 1){
+        clearInterval(id);
+    }else{
+        pos++;
+        point.style.top = pos + "px";
+        point.style.left = pos + "px";
+    }
 }
-}
+}  
+
 
 function moveLeft(){
 let position = 460;    
-let id = setInterval(frame, 5);    
+let id = setInterval(frame, 4);    
     function frame(){   
-        if(position == 640) {  
+        if(position == 460) {  
             if(hide.style.visibility = "visible"){
-            hide.style.visibility = "hidden";
-            hide.style.opacity = "0";
-            $("movebox").fadeToggle("fast", "linear");
-            if(show.style.visibility = "hidden"){
-            show.style.visibility = "visible";
-            show.style.opacity = "1";
-            $("showbox").fadeToggle("fast", "linear");      
+                hide.style.visibility = "hidden";
+                hide.style.opacity = "0";
+                $("movebox").fadeToggle("fast", "linear");
+                if(show.style.visibility = "hidden"){
+                    show.style.visibility = "visible";
+                    show.style.opacity = "1";
+                    $("showbox").fadeToggle("fast", "linear");      
         }
     }
 }
-if (position == 820) {
+if (position == 640) {
     clearInterval(id);
 } else { 
 position++;
@@ -50,3 +60,8 @@ hide.style.right = position + 'px';
 }
 }
 
+/* $("signBtn").click(function() {
+    $("movebox").fadeToggle("slow", "linear");
+    $("showbox").fadeToggle("slow", "linear");
+});
+ */
