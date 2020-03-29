4926177574d4d53179252df23e2f52ff9f677089
Julia.Zorina <julia.zorinaa@gmail.com>
2020-03-29 12:29:13 +0300

    added fadeToggle

--- mag.js
+++ mag.js
@@ -2,6 +2,7 @@
 let signBtn = document.getElementById('signbtn');
 /* let bgBox = document.getElementsByClassName('bgbox'); */
 let hide = document.getElementById('movebox');
+let show = document.getElementById('showbox');
 let blockNoAccount = document.getElementsByClassName('blockone')[0];
 
 signBtn.addEventListener('click', moveLeft);
@@ -29,35 +30,17 @@ let position = 460;
 let id = setInterval(frame, 5);    
     function frame(){   
         if(position == 640) {  
- /*        document.getElementsById('movebox'); */
-        let hide = document.getElementById('movebox');
             if(hide.style.visibility = "visible"){
-                hide.style.visibility = "hidden";
-                hide.style.opacity = "0";
-                    $("movebox").fadeToggle("slow", "linear");
-                var show = document.getElementById('showbox');
-                if(show.style.visibility = "hidden"){
-                    show.style.visibility = "visible";
-                    show.style.opacity = "1";
-                    $("showbox").fadeToggle("slow", "linear");      
+            hide.style.visibility = "hidden";
+            hide.style.opacity = "0";
+            $("movebox").fadeToggle("fast", "linear");
+            if(show.style.visibility = "hidden"){
+            show.style.visibility = "visible";
+            show.style.opacity = "1";
+            $("showbox").fadeToggle("fast", "linear");      
         }
     }
 }
-
-/*     while (bgBox.firstChild){
-    bgBox.removeChild(bgBox.firstChild);
-    }
-    signBtn.addEventListener('click', changeView);
-    function changeView() {
-    const newbox = document.createElement('div');  
-    newbox.classList.add('blocktwo');
-    bgBox.appendChild(newbox);
-    } */
-/*     const newbox = document.createElement('div');
-    newbox.classList = 'showbox';
-    newbox.innerHTML = 
-    let newElem = document.getElementById('showbox');
-    bgBox.replaceChild (newbox, box); */
 if (position == 820) {
     clearInterval(id);
 } else { 
@@ -67,8 +50,3 @@ hide.style.right = position + 'px';
 }
 }
 
-/* $("signBtn").click(function() {
-    $("movebox").fadeToggle("slow", "linear");
-    $("showbox").fadeToggle("slow", "linear");
-});
- */
