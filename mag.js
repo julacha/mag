966d9d7e9629f8ac1311c12116a1a5e9b56913b1
Julia.Zorina <julia.zorinaa@gmail.com>
2020-03-30 12:06:05 +0300

    added function frame

--- mag.js
+++ mag.js
@@ -2,38 +2,50 @@
 let signBtn = document.getElementById('signbtn');
 let hide = document.getElementById('movebox');
 let show = document.getElementById('showbox');
-let blockNoAccount = document.getElementsByClassName('blockone')[0];
+
 
 signBtn.addEventListener('click', moveLeft);
 signBtn.addEventListener('click', changeColor);
 
-function changeColor(id, speed){  
-    let point = document.getElementById('throw');
-        var right = parseInt(point.style.left) || 0;
-        var bottom = parseInt(point.style.top) || 0;
-        
-        if(point.offsetTop > window.innerHeight) right = bottom = 0 
-        else right = bottom += speed;
-        
-        point.style.left = right + "px";
-        point.style.top =  bottom + "px";
-    
-        var move = setTimeout(() => move45(id, speed), 50);
+/* dx=8;
+dy=3
+st=false //остановить процесс
+function Start_stop()
+{ if(st)
+  { window.clearInterval(proc) //остановиться
+    st=false //заменить надпись на кнопке с именем В1
+  }
+  else //двигаться (запустить процесс с именем proc)
+  { proc=window.setInterval("move()",100)
+  }
 }
-    move45('test', 1);
-/*     var pos = 0;
-    var id = setInterval(frame,5);
-    function frame(){ 
-    if(pos == 50){
-        clearInterval(id);
-    }else{
-        pos--;
-        point.style.top = pos + "px";
-        point.style.left = pos + "px";
-    }
+function move()
+let point = document.getElementById('throw');
+{ //ордината верхнего края фотографии (переведено
+//в тип INTEGER)
+var y=parseInt(document.all.point.style.top)
+//абсцисса левого края фотографии
+var x=parseInt(document.all.point.style.left)
+document.all.point.style.top=y+dy //Перемещение по OY
+document.all.point.style.left=x+dx //Перемещение по OX
 } */
 
 
+function changeColor(){  
+    let point = document.getElementById('throw');
+     let id = setInterval(frame,5);
+    function frame(){ 
+    if(pos == 100){
+        clearInterval(id);
+        signBtn.classList.add('color_selected');
+    }else{
+        pos++;
+        point.style.top = pos + "px";
+        point.style.left = pos + "px";
+    }
+}
+
+}
 
 function moveLeft(){
 let position = 460;    
@@ -59,9 +71,3 @@ hide.style.right = position + 'px';
 }
 }
 }
-
-/* $("signBtn").click(function() {
-    $("movebox").fadeToggle("slow", "linear");
-    $("showbox").fadeToggle("slow", "linear");
-});
- */
